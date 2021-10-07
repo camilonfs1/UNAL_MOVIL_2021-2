dec25760edce64b0de5d291f176c34821d262ffd
camilonfs1 <camilonfs1@gmail.com>
2021-10-07 18:40:45 -0500

    :spakles: Add App Project

---
+++ 20212PDDM/app/src/main/java/com/unal/a2021_2_pddm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.unal.a2021_2_pddm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
